b8ce3ef13a58bac03ec38d581cbeae595a5bca0a
Alexis Pennel <alexispennel@gmail.com>
2023-03-14 18:11:34 -0600

    sauvegarde plan

--- src/pages/MyPlan.jsx
+++ src/pages/MyPlan.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../api';
 import DaysBox from '../components/DaysBox';
@@ -10,6 +10,8 @@ const arrowLeft = <FontAwesomeIcon icon={faArrowLeft} className={styles.arrowLef
 
 const MyPlan = () => {
     const [recipes, setRecipes] = useState([]);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         api.get("/api/recipes")
@@ -18,6 +20,42 @@ const MyPlan = () => {
             })
     }, [])
 
+    const generatePlan = () => {
+        const data = {
+            mondayBreakfast: JSON.parse(localStorage.getItem('MondayBreakfast')).id,
+            mondayLunch: JSON.parse(localStorage.getItem('MondayLunch')).id,
+            mondayDinner: JSON.parse(localStorage.getItem('MondayDinner')).id,
+            tuesdayBreakfast: JSON.parse(localStorage.getItem('TuesdayBreakfast')).id,
+            tuesdayLunch: JSON.parse(localStorage.getItem('TuesdayLunch')).id,
+            tuesdayDinner: JSON.parse(localStorage.getItem('TuesdayDinner')).id,
+            wednesdayBreakfast: JSON.parse(localStorage.getItem('WednesdayBreakfast')).id,
+            wednesdayLunch: JSON.parse(localStorage.getItem('WednesdayLunch')).id,
+            wednesdayDinner: JSON.parse(localStorage.getItem('WednesdayDinner')).id,
+            thursdayBreakfast: JSON.parse(localStorage.getItem('ThursdayBreakfast')).id,
+            thursdayLunch: JSON.parse(localStorage.getItem('ThursdayLunch')).id,
+            thursdayDinner: JSON.parse(localStorage.getItem('ThursdayDinner')).id,
+            fridayBreakfast: JSON.parse(localStorage.getItem('FridayBreakfast')).id,
+            fridayLunch: JSON.parse(localStorage.getItem('FridayLunch')).id,
+            fridayDinner: JSON.parse(localStorage.getItem('FridayDinner')).id,
+            saturdayBreakfast: JSON.parse(localStorage.getItem('SaturdayBreakfast')).id,
+            saturdayLunch: JSON.parse(localStorage.getItem('SaturdayLunch')).id,
+            saturdayDinner: JSON.parse(localStorage.getItem('SaturdayDinner')).id,
+            sundayBreakfast: JSON.parse(localStorage.getItem('SundayBreakfast')).id,
+            sundayLunch: JSON.parse(localStorage.getItem('SundayLunch')).id,
+            sundayDinner: JSON.parse(localStorage.getItem('SundayDinner')).id,
+        }
+
+        setLoading(true)
+        api.post('/api/plans', data)
+            .then(() => {
+                setError(false);
+                setLoading(false)
+            })
+            .catch((error) => {
+                setLoading(false)
+                setError('Select all meals before planning. Only one plan can be created');
+            })
+    };
 
     return (
         <div className={styles.container}>
@@ -26,7 +64,7 @@ const MyPlan = () => {
                 <p>Go back</p>
             </Link>
             <section>
-                <h1>Click and select your meal</h1>
+                <h1>Select all your meals and click on 'generate plan'</h1>
                 <DaysBox day={'Monday'} data={recipes} />
                 <DaysBox day={'Tuesday'} data={recipes} />
                 <DaysBox day={'Wednesday'} data={recipes} />
@@ -35,7 +73,10 @@ const MyPlan = () => {
                 <DaysBox day={'Saturday'} data={recipes} />
                 <DaysBox day={'Sunday'} data={recipes} />
             </section>
-        </div>
+            {error === '' ? null : <p>{error}</p>}
+            {loading && <p>Loading...</p>}
+            <button className={styles.button} onClick={generatePlan}>Generate plan</button>
+        </div >
     );
 };
 
