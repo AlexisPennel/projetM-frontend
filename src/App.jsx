9c91ab8c70bbfc688736adb450b5103aea6d711b
Alexis Pennel <alexispennel@gmail.com>
2023-03-17 10:32:42 -0600

    affichage plan du jour / loading data dashboard / gestion erreurs

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import ErrorPage from './pages/ErrorPage';
 import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom';
 import BaseLayout from './components/BaseLayout';
 import Dashboard from './pages/Dashboard';
-import { getRecipesData } from './pages/Dashboard'
 import Recipes from './pages/Recipes';
+import CreatePlan from './pages/CreatePlan';
 import MyPlan from './pages/MyPlan';
 
 
@@ -16,9 +16,10 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <Route element={<BaseLayout />} errorElement={<ErrorPage />}>
             <Route path='/' element={<HomePage />} />
-            <Route path='/dashboard' element={token === null ? <Navigate to={'/'} /> : <Dashboard />} loader={getRecipesData} />
+            <Route path='dashboard' element={token === null ? <Navigate to={'/'} /> : <Dashboard />} />
             <Route path='recipes/:id' element={token === null ? <Navigate to={'/'} /> : <Recipes />} />
-            <Route path='plan' element={token === null ? <Navigate to={'/'} /> : <MyPlan />} />
+            <Route path='plan/' element={token === null ? <Navigate to={'/'} /> : <MyPlan />} />
+            <Route path='plan/createplan' element={token === null ? <Navigate to={'/'} /> : <CreatePlan />} />
         </Route>
     )
 );
